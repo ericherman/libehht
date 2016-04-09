8b3c4e7e5cd90ceb95765a7015117ed35d1aee9b
Eric Herman <eric@freesa.org>
2016-04-09 19:22:46 +0200

    show how to convert to array

---
+++ ehht-as-array.c
@@ -0,0 +1,80 @@
+#include <stdio.h>		/* fprintf fscanf printf */
+#include <stdlib.h>		/* malloc */
+#include <string.h>		/* strlen */
+#include <strings.h>		/* strdup */
+
+#include "ehht.h"
+
+#define MAKE_VALGRIND_HAPPY 1
+
+struct kva_s {
+	size_t pos;
+	size_t len;
+	char **keys;
+	size_t *lens;
+	void **vals;
+};
+
+int to_array(const char *key, size_t len, void *val, void *ctx)
+{
+	struct kva_s *kva;
+	char *str;
+
+	kva = ctx;
+
+	str = strdup(key);
+
+	kva->keys[kva->pos] = str;
+	kva->lens[kva->pos] = strlen(str);
+	kva->vals[kva->pos] = val;
+
+	++kva->pos;
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	struct ehht_s *table;
+	size_t i, num_buckets;
+	struct kva_s kva;
+
+	num_buckets = (argc > 1) ? atoi(argv[1]) : 0;
+
+	table = ehht_new(num_buckets, NULL, NULL, NULL, NULL);
+	if (table == NULL) {
+		fprintf(stderr, "ehht_new returned NULL");
+		exit(EXIT_FAILURE);
+	}
+
+	table->put(table, "foo", strlen("foo"), "f");
+	table->put(table, "bar", strlen("bar"), "b");
+	table->put(table, "to", strlen("to"), "t");
+	table->put(table, "whiz", strlen("whiz"), "w");
+	table->put(table, "bang", strlen("bang"), "b");
+
+	kva.len = table->size(table);
+	kva.pos = 0;
+	kva.keys = malloc(sizeof(char *) * kva.len);
+	kva.lens = malloc(sizeof(size_t) * kva.len);
+	kva.vals = malloc(sizeof(void *) * kva.len);
+
+	table->for_each(table, to_array, &kva);
+
+	for (i = 0; i < kva.pos; ++i) {
+		printf("kva.keys[%u]=%s, kva.lens[%u]:%u, kva.vals[%i]:%s\n",
+		       (unsigned)i, kva.keys[i], (unsigned)i, kva.lens[i],
+		       (unsigned)i, (char *)kva.vals[i]);
+	}
+
+	if (MAKE_VALGRIND_HAPPY) {
+		for (i = 0; i < kva.pos; ++i) {
+			free(kva.keys[i]);
+		}
+		free(kva.keys);
+		free(kva.lens);
+		free(kva.vals);
+		ehht_free(table);
+	}
+	return 0;
+}
