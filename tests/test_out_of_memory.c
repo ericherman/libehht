77ad07657000c0cbbb9496f162bf99bc2925d57b
Eric Herman <eric@freesa.org>
2020-02-01 13:50:52 +0100

    test more OOM conditions

--- tests/test_out_of_memory.c
+++ tests/test_out_of_memory.c
@@ -31,6 +31,102 @@ int test_out_of_memory_construction(unsigned allocs_to_fail)
 	return failures;
 }
 
+int test_out_of_memory_put(unsigned allocs_to_fail)
+{
+	int failures = 0;
+	struct ehht_s *table;
+	struct tracking_mem_context ctx;
+	size_t i;
+	int err;
+	char buf[80];
+
+	memset(&ctx, 0x00, sizeof(struct tracking_mem_context));
+	ctx.attempts_to_fail_bitmask = allocs_to_fail;
+
+	table = ehht_new_custom(0, NULL, test_malloc, test_free, &ctx);
+	if (!table) {
+		fprintf(stderr, "%s:%d: ehht_new_custom\n", __FILE__, __LINE__);
+		return 1;
+	}
+
+	err = 0;
+	for (i = 0; i < 10; ++i) {
+		sprintf(buf, "%u", (unsigned)i);
+		table->put(table, buf, strlen(buf), NULL, &err);
+		if (err && !allocs_to_fail) {
+			++failures;
+		}
+	}
+	if (allocs_to_fail && !err) {
+		++failures;
+	}
+
+	ehht_free(table);
+
+	failures += check_unsigned_int_m(ctx.frees, ctx.allocs, "alloc/free");
+	failures +=
+	    check_unsigned_int_m(ctx.free_bytes, ctx.alloc_bytes, "bytes");
+
+	return failures;
+}
+
+int test_out_of_memory_keys(unsigned allocs_to_fail)
+{
+	int failures = 0;
+	struct ehht_s *table;
+	struct tracking_mem_context ctx;
+	size_t i;
+	int err, allocate_copies;
+	char buf[80];
+	struct ehht_keys_s *ks;
+
+	memset(&ctx, 0x00, sizeof(struct tracking_mem_context));
+	ctx.attempts_to_fail_bitmask = allocs_to_fail;
+
+	table = ehht_new_custom(0, NULL, test_malloc, test_free, &ctx);
+	if (!table) {
+		fprintf(stderr, "%s:%d: ehht_new_custom\n", __FILE__, __LINE__);
+		return 1;
+	}
+
+	err = 0;
+	for (i = 0; i < 10; ++i) {
+		sprintf(buf, "%u", (unsigned)i);
+		table->put(table, buf, strlen(buf), NULL, &err);
+		if (err) {
+			fprintf(stderr, "%s:%d: put %u\n", __FILE__, __LINE__,
+				(unsigned)i);
+			return 1;
+		}
+	}
+
+	err = 0;
+	for (i = 0; i < 10; ++i) {
+		allocate_copies = 1;
+		ks = table->keys(table, allocate_copies);
+		if (!ks && !allocs_to_fail) {
+			fprintf(stderr, "%s:%d: !ks %u\n", __FILE__, __LINE__,
+				(unsigned)i);
+			return 1;
+		}
+		if (ks) {
+			table->free_keys(table, ks);
+		} else {
+			++err;
+		}
+	}
+	failures += (!err && allocs_to_fail) ? 1 : 0;
+	failures += (err && !allocs_to_fail) ? 1 : 0;
+
+	ehht_free(table);
+
+	failures += check_unsigned_int_m(ctx.frees, ctx.allocs, "alloc/free");
+	failures +=
+	    check_unsigned_int_m(ctx.free_bytes, ctx.alloc_bytes, "bytes");
+
+	return failures;
+}
+
 int test_out_of_memory(void)
 {
 	int failures = 0;
@@ -39,6 +135,19 @@ int test_out_of_memory(void)
 	failures += test_out_of_memory_construction(1 << 0);
 	failures += test_out_of_memory_construction(1 << 1);
 	failures += test_out_of_memory_construction(1 << 2);
+
+	failures += test_out_of_memory_put(0);
+	failures += test_out_of_memory_put(1 << 4);
+	failures += test_out_of_memory_put(1 << 5);
+	failures += test_out_of_memory_put(1 << 6);
+	failures += test_out_of_memory_put(1 << 7);
+
+	failures += test_out_of_memory_keys(0);
+	failures += test_out_of_memory_keys(1 << 23);
+	failures += test_out_of_memory_keys(1 << 24);
+	failures += test_out_of_memory_keys(1 << 25);
+	failures += test_out_of_memory_keys(1 << 27);
+	failures += test_out_of_memory_keys(1 << 28);
 
 	return failures;
 }
